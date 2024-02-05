ce697c5301c1ce6d7aebcae9e89853a75dcfd022
piitulaineniikka <iikkap.ip@gmail.com>
2024-02-05 15:09:45 +0200

    add lang

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,10 @@ export default defineNuxtConfig({
 				{ charset: "utf-8" },
 				{ name: "viewport", content: "width=device-width, initial-scale=1" },
 				{ hid: "description", name: "description", content: "Iikka Piitulainen portfolio" },
-			]
+			],
+			htmlAttrs: {
+				lang: "fi",
+			},
 		},
 	},
 });
