575444f15ce0cb34082beb01fd7d39bb70f5d943
piitulaineniikka <iikkap.ip@gmail.com>
2024-02-05 15:04:28 +0200

    change app head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,15 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ["@nuxtjs/tailwindcss"],
 	css: ["~/assets/styles/index.css"],
-	ssr: true
+	ssr: true,
+	app: {
+		head: {
+			title: "Iikka Piitulainen - Portfolio",
+			meta: [
+				{ charset: "utf-8" },
+				{ name: "viewport", content: "width=device-width, initial-scale=1" },
+				{ hid: "description", name: "description", content: "Iikka Piitulainen portfolio" },
+			]
+		},
+	},
 });
