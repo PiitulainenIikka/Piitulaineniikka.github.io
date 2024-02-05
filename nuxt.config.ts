6512ac2f80af500bc680fb44a5d2aa5698697948
piitulaineniikka <iikkap.ip@gmail.com>
2024-02-05 12:54:27 +0200

    add title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,13 @@ export default defineNuxtConfig({
 	css: ["~/assets/styles/main.css"],
 	title: "Portfolio",
 	ssr: true,
+	// add title to head 
+	head: {
+		title: "Iikka Piitulainen - Portfolio",
+		meta: [
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{ hid: "description", name: "description", content: "Iikka Piitulainen - Software Developer and Portfolio" },
+		],
+	},
 });
