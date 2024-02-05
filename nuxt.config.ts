3185293c6e21305be302c04db3136f7c7dfd23cd
piitulaineniikka <iikkap.ip@gmail.com>
2024-02-05 13:37:31 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,5 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ["@nuxtjs/tailwindcss"],
 	css: ["~/assets/styles/main.css"],
-	ssr: true,
-	app: {
-		head: {
-			title: "Iikka Piitulainen - Portfolio",
-			link: [
-				{ rel: 'stylesheet', href: '~/assets/styles/main.css' }
-			],
-		},
-	},
+	ssr: true
 });
