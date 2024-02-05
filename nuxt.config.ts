516177c94888b48f4416e3998f1b0ffabf5b9b1a
piitulaineniikka <iikkap.ip@gmail.com>
2024-02-05 20:13:34 +0200

    revert back

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,23 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ["@nuxtjs/tailwindcss"],
-	css: ["~/assets/styles/main.css"],
-	title: "Portfolio",
+	css: ["~/assets/styles/index.css"],
 	ssr: true,
+	app: {
+		head: {
+			title: "Iikka Piitulainen - Portfolio",
+			meta: [
+				{ charset: "utf-8" },
+				{ name: "viewport", content: "width=device-width, initial-scale=1" },
+				{
+					hid: "description",
+					name: "description",
+					content: "Iikka Piitulainen portfolio",
+				},
+			],
+			htmlAttrs: {
+				lang: "fi",
+			},
+		},
+	},
 });
